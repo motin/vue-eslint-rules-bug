b700527c37eea096d1a0511b0488830d44b2c54e
Fredrik Wollsén <fredrik@neam.se>
2018-09-24 01:50:34 +0300

    Added template code to App.vue that is invalid according to the essential vue/no-use-v-if-with-v-for rule

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+      v-for="(foo, key) of bar"
+      v-if="foo"
+      :key="key">
+      Foo
+    </div>
   </div>
 </template>
 
